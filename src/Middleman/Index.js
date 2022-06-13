7a781297e2ab844f67ab0652d494a1086dd9338f
YexuanGuo <yexuan.guo@gmail.com>
2022-06-13 17:39:03 +0800

    状态提升，非父子通信

---
+++ src/Middleman/Index.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import axios from "axios";
+
+export default class Index extends Component {
+    constructor() {
+        super();
+        this.state = {
+            filmList: [],
+            detailInfo: {
+                name: "",
+                info: ""
+            }
+        }
+        axios({
+            url: "https://m.maizuo.com/gateway?cityId=110100&pageNum=1&pageSize=10&type=1&k=5253884",
+            headers: {
+                "X-Client-Info": '{"a":"3000","ch":"1002","v":"5.2.0","e":"16547570291073282262499329","bc":"110100"}',
+                'X-Host': 'mall.film-ticket.film.list'
+            },
+        }).then(res=>{
+            this.setState({
+                filmList: res.data.data.films
+            })
+        }).catch(err=>{
+            console.log("error:"+err)
+        })
+    }
+    render() {
+        return (
+            <div>
+                {
+                    this.state.filmList.map((item)=>
+                        <FilmItem key={item.filmId} {...item} onItemEvent={(name, val)=>{
+                            this.setState({
+                                detailInfo: {
+                                    name: name,
+                                    info: val
+                                }
+                            })
+                        }}/>
+                    )
+                }
+                <FilmDetail detailInfo={this.state.detailInfo}/>
+            </div>
+        );
+    }
+}
+
+
+class FilmItem extends Component {
+    render() {
+        let {name, poster, synopsis} = this.props
+        return (
+            <div onClick={(currentEvt)=>{
+                this.props.onItemEvent(name, synopsis)
+            }}>
+                <img src={poster} alt={name} style={{height:200, width:200}}/>
+                {name}
+            </div>
+        );
+    }
+}
+
+class FilmDetail extends Component{
+    render() {
+        return(
+            <div style={{height:300, width:300, background:"#ccc", top:100, position:"fixed", right:0}}>
+                {this.props.detailInfo.name} - {this.props.detailInfo.info}
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import ControlLogin from "./User/ControlLogin";
+import Index from "./Middleman/Index";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <div>
-        <ControlLogin/>
+        <Index/>
     </div>
 );
